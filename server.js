bd8b3070ddef8318264520ef417d681188ee05cc
niko132 <nikokirste@hotmail.de>
2018-04-22 14:19:28 +0200

    .

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res, next) {
+	console.log('Request to ' + req.url);
+	res.send('Hello World!');
+	next();
+});
+
+app.listen(port, function() {
+	console.log('Example app listening on port ' + port);
+});
